9fdd7d46c7b73462cc054986d58e2d94a43a9cf3
Coleman <penguincoder@gmail.com>
2008-08-07 17:45:33 -0500

    better safe url checking

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,25 @@
 class Comment < ActiveRecord::Base
   belongs_to :page
   belongs_to :author
+  validate :safe_url
   
   def name
     if self.author
       self.author.name
     else
       self.user
+    end
+  end
+  
+  private
+  
+  def safe_url
+    return true if self.url.to_s.empty?
+    if self.url =~ /^http:\/\// and self.url !~ /[^a-zA-Z0-9\._:\-\/]/
+      true
+    else
+      self.errors.add(:url, "is not a permissible address")
+      false
     end
   end
 end
